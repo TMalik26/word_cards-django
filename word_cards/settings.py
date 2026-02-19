dd444c9e5aab131e9fadf0ea2e9dcbe561de85c8
TMalik26 <maliktetianamykolaivna@gmail.com>
2026-02-19 17:30:27 +0200

    Update STATICFILES_STORAGE to use CompressedStaticFilesStorage for improved performance

--- word_cards/settings.py
+++ word_cards/settings.py
@@ -138,7 +138,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
